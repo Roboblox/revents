17186e1e987a942311f43420acbeded57a236c05
roboblox <schulerjohnthomas@gmail.com>
2018-06-22 17:01:18 -0700

    enmd of sec 6

--- src/features/user/Settings/SettingsNav.jsx
+++ src/features/user/Settings/SettingsNav.jsx
@@ -1,9 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
+import { Grid, Header, Menu } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
-export class SettingsNav extends Component {
-  render() {
-    return <div>Settings Nav</div>;
-  }
-}
+const SettingsNav = () => {
+  return (
+    <Grid.Column width={4}>
+      <Menu vertical>
+        <Header icon="user" attached inverted color="grey" content="Profile" />
+        <Menu.Item as={NavLink} to="/settings/basic">
+          {" "}
+          Basics
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/settings/about">
+          {" "}
+          About me
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/settings/photos">
+          {" "}
+          My Photos
+        </Menu.Item>
+      </Menu>
+      <Grid.Row />
+      <Menu vertical>
+        <Header
+          icon="settings"
+          attached
+          inverted
+          color="grey"
+          content="Account"
+        />
+        <Menu.Item as={NavLink} to="/settings/account">
+          My Account
+        </Menu.Item>
+      </Menu>
+    </Grid.Column>
+  );
+};
 
 export default SettingsNav;
