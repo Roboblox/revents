58f7add3b179abb0ad27c8acf1cd07e98a3f1697
roboblox <schulerjohnthomas@gmail.com>
2018-06-25 17:45:35 -0700

    section 11 forgot to push This is registerform section 11 lecture 98

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
 import { connect } from "react-redux";
+import { Button } from "semantic-ui-react";
+import Script from "react-load-script";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 import { incrementCounter, decrementCounter } from "./testActions";
+import { openModal } from "../modals/modalActions";
 
 const mapState = state => ({
   data: state.test.data
@@ -9,18 +15,66 @@ const mapState = state => ({
 
 const actions = {
   incrementCounter,
-  decrementCounter
+  decrementCounter,
+  openModal
 };
 
 class TestComponent extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+  state = {
+    address: "",
+    scriptLoaded: false
+  };
+  handleScriptLoad = () => {
+    this.setState({ scriptLoaded: true });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
+
+  onChange = address => this.setState({ address });
+
   render() {
-    const { incrementCounter, decrementCounter, data } = this.props;
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange
+    };
+    const { incrementCounter, decrementCounter, data, openModal } = this.props;
     return (
       <div>
+        <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyDvE_V9JsbPNzeFKpRe2SoJa_t0YeKHuI8&libraries=places"
+          onLoad={this.handleScriptLoad}
+        />
         <h1> Test Area</h1>
         <h3>The answer is: {data}</h3>
         <Button onClick={incrementCounter} color="green" content="Increment" />
         <Button onClick={decrementCounter} color="red" content="Decrement" />
+        <Button
+          onClick={() => openModal("TestModal", { data: 43 })}
+          color="teal"
+          content="Open Modal"
+        />
+        <br />
+        <br />
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.scriptLoaded && (
+            <PlacesAutocomplete inputProps={inputProps} />
+          )}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
